6ef77c169b8db9affb2ba5b6e93942f57e14337e
IrisTc <1010939908@qq.com>
2020-05-06 09:32:30 +0800

    add encrypt

--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,45 @@
 const router = require('koa-router')()
 const Users = require('../models/users')
+const bcrypt = require('bcryptjs')
 
 router.prefix('/user')
 
 router.post('/login', async (ctx, next) => {
-    ctx.set('Access-Control-Allow-Credentials', true);
-    data = {
-        "name": ctx.query.name,
-        "password": ctx.query.password
-    }
-    let doc = await Users.find(data)
-    if(doc.length !== 0){
-        ctx.session.user = {
-            _id: doc._id,
-            uername: doc.name
+    let data = ctx.request.body
+    let user = await Users.findOne({name: data.name})
+    console.log(user)
+    if(user !== null){
+        let confirmRes = bcrypt.compareSync(data.password, user.password)
+        if(confirmRes){
+            ctx.session.user = {
+                _id: user._id,
+                uername: user.name
+            }
+            ctx.body = {
+                status: '200',
+                msg: 'login successfully',
+                result: user
+            }
         }
     }
-    ctx.body = {
-        status: '200',
-        msg: '',
-        result: doc
-    }
+    
 })
 
+router.post('/register', async (ctx, next)=>{
+    let data = ctx.request.body
+    let salt = bcrypt.genSaltSync(10);
+    data.password = bcrypt.hashSync(data.password, salt);
+    await Users.create(data).then(()=>{
+        ctx.body = {
+            status: '200',
+            user: data,
+            msg: 'register successfully'
+        }
+    }).catch(()=>{
+        ctx.body = {
+            status: '500',
+            msg: 'register'
+        }
+    })
+})
 module.exports = router
